fb711cae6242c089ee8f0d3975d3b6d4c018ad67
seanshli <seanshli@gmail.com>
2025-10-26 11:55:41 +0800

    Add iOS cookie storage configuration for authentication

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import Foundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,6 +8,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Configure HTTP cookie storage for authentication
+        HTTPCookieStorage.shared.cookieAcceptPolicy = .always
+        
         // Override point for customization after application launch.
         return true
     }

0fc167a2084f757db1a255ad5d2f1fe6f5e32646
seanshli <seanshli@gmail.com>
2025-12-10 22:51:40 +0800

    fix: Add header guards to bridging header

--- ios/App/App/App-Bridging-Header.h
+++ ios/App/App/App-Bridging-Header.h
@@ -5,4 +5,9 @@
 //  Bridging header to expose Capacitor macros to Swift
 //
 
+#ifndef App_Bridging_Header_h
+#define App_Bridging_Header_h
+
 #import <Capacitor/Capacitor.h>
+
+#endif /* App_Bridging_Header_h */
